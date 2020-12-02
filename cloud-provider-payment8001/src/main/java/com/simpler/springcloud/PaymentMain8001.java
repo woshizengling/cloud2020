623d62d0b1163e3bb11dc2254c1eccb887062479
simpler <343094379@qq.com>
2020-12-02 18:16:56 +0800

    ..

--- cloud-provider-payment8001/src/main/java/com/simpler/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/simpler/springcloud/PaymentMain8001.java
@@ -1,10 +1,19 @@
 package com.simpler.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author simpler
  * @program: springcloud_study
  * @description:
  * @create 2020/12/02 18:01
  */
+@SpringBootApplication
 public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
 }
