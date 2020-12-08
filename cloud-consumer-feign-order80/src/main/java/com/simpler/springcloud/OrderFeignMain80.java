de1ffa3e6848cf69772032a69c82c693432e74c8
simpler <343094379@qq.com>
2020-12-08 11:25:37 +0800

    openFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/simpler/springcloud/OrderFeignMain80.java
@@ -0,0 +1,21 @@
+package com.simpler.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author simpler
+ * @program: cloud2020
+ * @description:
+ * @create 2020/12/08 10:30
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/simpler/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,24 @@
+package com.simpler.springcloud.service;
+
+import com.simpler.springcloud.entities.CommonResult;
+import com.simpler.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author simpler
+ * @program: cloud2020
+ * @description:
+ * @create 2020/12/08 10:34
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+
+}
